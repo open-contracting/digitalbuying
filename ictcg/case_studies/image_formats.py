47d71e5ccda0f4b59114f43a3e38220d8115567a
Leigh Johnson <leigh.johnson@wearefriday.com>
2020-07-17 14:28:33 +0100

    Update to rich text caption.  Added mark_safe to alt desc to prevent un-needed escaping.  Minor update to style to fix linting.

--- ictcg/case_studies/image_formats.py
+++ ictcg/case_studies/image_formats.py
@@ -1,10 +1,10 @@
-from django.utils.html import format_html
+from django.utils.html import format_html, mark_safe
 from wagtail.images.formats import Format, register_image_format
 
 class CaptionedImageFormat(Format):
     def image_to_html(self, image, alt_text, extra_attributes=None):
         default_html = super().image_to_html(image, '', extra_attributes)
-        return format_html("<figure>{}<figcaption>{}</figcaption></figure>", default_html, alt_text)
+        return format_html("<figure>{}<figcaption>{}</figcaption></figure>", default_html, mark_safe(alt_text))
 
 register_image_format(
     CaptionedImageFormat('captioned_fullwidth', 'Full width captioned', 'richtext-image full-width', 'width-800')
