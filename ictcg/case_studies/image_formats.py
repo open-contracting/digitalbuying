01aa3b349616e1813182884afc7520ecb4a1ae66
Leigh Johnson <leigh.johnson@wearefriday.com>
2020-07-17 13:50:33 +0100

    Added caption format to rich text images. Added style for figcaption.

---
+++ ictcg/case_studies/image_formats.py
@@ -0,0 +1,11 @@
+from django.utils.html import format_html
+from wagtail.images.formats import Format, register_image_format
+
+class CaptionedImageFormat(Format):
+    def image_to_html(self, image, alt_text, extra_attributes=None):
+        default_html = super().image_to_html(image, '', extra_attributes)
+        return format_html("<figure>{}<figcaption>{}</figcaption></figure>", default_html, alt_text)
+
+register_image_format(
+    CaptionedImageFormat('captioned_fullwidth', 'Full width captioned', 'richtext-image full-width', 'width-800')
+)
