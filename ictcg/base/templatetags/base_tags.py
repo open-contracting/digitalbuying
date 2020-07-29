06ff5c5fb172b815787d2e92471146e15cbce4a3
Leigh Johnson <leigh.johnson@wearefriday.com>
2020-07-29 15:29:06 +0100

    Minor fixes: Remove unused packages from package.json Updated base template tags to avoid error when request is not set within context.

--- ictcg/base/templatetags/base_tags.py
+++ ictcg/base/templatetags/base_tags.py
@@ -5,10 +5,14 @@ register = template.Library()
 
 @register.simple_tag(takes_context = True)
 def get_cookie_notice(context):
-    request = context['request']
+    request = context.get('request')
+    if request is None:
+        return True
     return request.COOKIES.get('cookie_notice')
 
 @register.simple_tag(takes_context = True)
 def get_analytics_cookie(context):
-    request = context['request']
+    request = context.get('request')
+    if request is None:
+        return False
     return request.COOKIES.get('analytics', 'false') == 'true'
