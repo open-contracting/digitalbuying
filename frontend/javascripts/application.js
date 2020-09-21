b6fd328e21ec70c2588e1fcbbd6ca4d83d8b5cd9
benvand <benjaminfvandersteen@gmail.com>
2020-09-21 22:59:51 +0100

    Move GA to top of page for page load time

--- frontend/javascripts/application.js
+++ frontend/javascripts/application.js
@@ -5,8 +5,6 @@ import Accordion from './components/accordion'
 import Header from './components/header'
 import Cookie from './components/cookie'
 import { Button } from 'govuk-frontend'
-import { getCookieByName } from './utils/cookie'
-import { loadGoogleAnalytics } from './google-analytics'
 
 (function () {
   var $accordions = document.querySelectorAll('[data-module="govuk-accordion"]')
@@ -33,15 +31,4 @@ import { loadGoogleAnalytics } from './google-analytics'
     new Cookie($cookiebanner).init()
   }
 
-  if (getCookieByName('analytics') === 'true') {
-    const gaID = document.getElementsByTagName('body')[0].getAttribute('data-ga-id')
-
-    loadGoogleAnalytics(gaID)
-    window.dataLayer = window.dataLayer || []
-
-    function gtag(){dataLayer.push(arguments)} /* eslint-disable-line */
-
-    gtag('js', new Date())
-    gtag('config', gaID)
-  }
 })()
--- frontend/javascripts/google-analytics.js
+++
@@ -1,8 +0,0 @@
-export const loadGoogleAnalytics = (gaID) => {
-  var ga = document.createElement('script')
-  ga.type = 'text/javascript'
-  ga.async = true
-  ga.src = `https://www.googletagmanager.com/gtag/js?id=${gaID}`
-  var s = document.getElementsByTagName('script')[0]
-  s.parentNode.insertBefore(ga, s)
-}
