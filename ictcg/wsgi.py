1dc4bbfa262c4a50de99170b7e634fb2ef6526cf
benvand <benjaminfvandersteen@gmail.com>
2020-10-23 12:35:16 +0100

    Make Translogger log to stdout not stderr

--- ictcg/wsgi.py
+++ ictcg/wsgi.py
@@ -8,8 +8,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 https://docs.pylonsproject.org/projects/waitress/en/stable/logging.html#access-logging
 """
+import logging
+import sys
+
 from django.core.wsgi import get_wsgi_application
 from paste.translogger import TransLogger
 
 
-application = TransLogger(get_wsgi_application())
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(logging.Formatter('%(message)s'))
+
+logger = logging.getLogger('wsgi')
+logger.addHandler(handler)
+logger.propagate = False
+logger.setLevel(logging.DEBUG)
+
+application = TransLogger(get_wsgi_application(), logger=logger)
