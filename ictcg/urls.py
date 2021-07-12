795967f28bf49520ec70b4b929ade2bfe639e87e
benvand <benjaminfvandersteen@gmail.com>
2021-07-12 17:15:48 +0100

    Add missing import to urls.py

--- ictcg/urls.py
+++ ictcg/urls.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.contrib.sitemaps.views import sitemap
+from wagtail.documents import urls as wagtaildocs_urls
 
 from health_check.views import MainView
 
