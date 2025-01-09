ef46d3b6f93d4d719eb4353d5c7a964d7de20776
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-01-09 10:39:42 -0500

    chore: Simplify accordion JS

--- frontend/javascripts/components/accordion.js
+++ frontend/javascripts/components/accordion.js
@@ -3,10 +3,8 @@ import { Accordion } from "govuk-frontend";
 // Update "Open all / Close all" button text to include translated versions
 Accordion.prototype.updateOpenAllButton = function (expanded) {
     const el = document.querySelector('[data-module="govuk-accordion"]');
-    let newButtonText = expanded ? el.getAttribute("data-close-text") : el.getAttribute("data-open-text");
-    newButtonText += '<span class="govuk-visually-hidden"> sections</span>';
     this.$openAllButton.setAttribute("aria-expanded", expanded);
-    this.$openAllButton.innerHTML = newButtonText;
+    this.$openAllButton.innerHTML = `${expanded ? el.getAttribute("data-close-text") : el.getAttribute("data-open-text")}<span class="govuk-visually-hidden"> sections</span>`;
 };
 
 export default Accordion;
