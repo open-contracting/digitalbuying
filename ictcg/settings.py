16a86e7623054027aa69c5345ffcdf56b563dac2
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-01-09 10:19:50 -0500

    feat: Add Fathom Analytics

--- ictcg/settings.py
+++ ictcg/settings.py
@@ -261,7 +261,7 @@ EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 
 FIXTURE_DIRS = [BASE_DIR / "tests" / "fixtures"]
 
