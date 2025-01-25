551ee33a5cef0968fc413291788f5ac65eba5977
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-01-24 20:01:10 -0500

    build(django32): Avoid autofield migrations https://docs.djangoproject.com/en/5.1/releases/3.2/#customizing-type-of-auto-created-primary-keys

--- navigation/apps.py
+++ navigation/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SponsorsConfig(AppConfig):
-    name = "sponsors"
+class ModulesConfig(AppConfig):
+    name = "navigation"
     default_auto_field = "django.db.models.AutoField"
